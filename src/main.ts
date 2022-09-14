d6550210f90bb6cc54af453048af67a12fc8c782
Oskar Puchalski <oskarpuchalski17@gmail.com>
2022-09-15 00:32:28 +0200

    Port is now taken from .env to work on railway

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const PORT = process.env.PORT;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT || 3000);
 }
 bootstrap();
